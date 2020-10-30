eff9d95b305e7b31ed30139afd7544509f2bdb83
anderson <anderson.d.cardoso92@gmail.com>
2020-10-30 17:52:41 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello React Lab</h1>
+            <p>This is my react experimentation environment</p>
+        </div>
+    );
+}
+
+export default App;
